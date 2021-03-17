593b184e006e6488efa6898f2e8d4e9a3a28bf57
krushikar543 <77056197+krushikar543@users.noreply.github.com>
2021-03-17 15:59:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,14 @@
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+
+
+
+
 import pygame
 import random
 import time
+
 
 class Snake():
     def __init__(self):
@@ -49,7 +54,7 @@ class Snake():
         else:
             self.display_text("Pay : " + str(300))
         pygame.quit()
-        quit()
+        # quit()
 
     def move(self):
         cur = self.head_position()
@@ -125,7 +130,7 @@ down = (0, 1)
 left = (-1, 0)
 right = (1, 0)
 
-def game():
+def snake_game():
     pygame.init()
 
     clock = pygame.time.Clock()
@@ -137,7 +142,7 @@ def game():
 
     snake = Snake()
     food = Food()
-
+    snake.display_text("WELCOME TO GAME-1")
     snake.display_text("THE SNAKE GAME")
 
     myfont = pygame.font.SysFont("monospace", 16)
@@ -165,14 +170,137 @@ def game():
         screen.blit(text, (20, 15))
         pygame.display.update()
 
-game()
+snake_game()
 
-'''
+import random
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode([500, 500])
+
+pygame.display.set_caption("Rock, Paper and Scissor")
+
+image = pygame.image.load(r'rps.jpg')
+
+playing = True
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+
+    window.fill((255, 255, 255))
+    window.blit(image, (0, 0))
+
+    pygame.display.update()
+
+pygame.quit()
+
+print()
+
+print("**************************************************")
+print("Welcome to Game Number 2 --- Rock,Paper or Scissor")
+print("**************************************************")
+
+print()
+x = 'Y'
+cost = 0
+while x == 'Y':
+    print("Play with a friend --- Press 1")
+    print("Play with computer --- Press 2")
+
+    print()
+
+
+    def game(p1, p2):
+        if p1 == p2:
+            return None
+
+        elif p2 == 's':
+            if p1 == 'p':
+                return False
+            elif p1 == 'r':
+                return True
+
+        elif p2 == 'r':
+            if p1 == 's':
+                return False
+            elif p1 == 'p':
+                return True
+
+        elif p2 == 'p':
+            if p1 == 'r':
+                return False
+            elif p1 == 's':
+                return True
+
+
+    i = int(input("Time To Play---"))
+
+    print()
+
+    opponent = random.randint(1, 3)
+
+    if i == 1:
+        player1 = input("Player1 Turn: Rock(r) Paper(p) or Scissor(s)---")
+        print()
+        player2 = input("Player2 Turn: Rock(r) Paper(p) or Scissor(s)---")
+        print()
+        a = game(player1, player2)
+
+        if a is None:
+            print("The game is a tie!")
+        elif a:
+            print("Player1 Wins!")
+        else:
+            print("Player2 Wins!")
+
+    elif i == 2:
+        print("Computer Turn: Rock(r) Paper(p) or Scissor(s)")
+
+        print()
+
+        if opponent == 1:
+            comp = 'r'
+
+        elif opponent == 2:
+            comp = 'p'
+
+        elif opponent == 3:
+            comp = 's'
+        player = input("Your Turn: Rock(r) Paper(p) or Scissor(s)---")
+        a = game(comp, player)
+        print(f"Computer chose {comp}")
+        print(f"You chose {player}")
+
+        if a is None:
+            print("The game is a tie!")
+        elif a:
+            print("You Lose!")
+        else:
+            print("You Win!")
+        print()
+
+    x = input("Wanna play again Yes(Y) or No(N)---")
+    print()
+
+    if x == 'N':
+        print("Thanks Sir/Mam!")
+        print()
+
+    cost += 100
+cost += 20
+
+print(f"Pay:{cost}")
+
+
 
 import pygame
 
 pygame.init()
 window = pygame.display.set_mode([720, 480])
+
+pygame.display.set_caption("Hangaroo")
 
 image = pygame.image.load(r'hangaroo.jpg')
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -181,7 +309,7 @@ text = font.render('My Inspiration', True, (0, 255, 0), (0, 0, 128))
 
 textrect = text.get_rect()
 
-textrect.center = (720//2, 100 // 2)
+textrect.center = (720 // 2, 100 // 2)
 
 playing = True
 
@@ -212,7 +340,7 @@ print("Please Wait")
 cost = 100
 
 
-def hangaroo():
+def hangaroo(res=None):
     global ss, ll, ss1, n, ffdata, temps, first
     first = inpp.get()
     input1.delete(0, END)
@@ -247,19 +375,19 @@ def hangaroo():
 
 
 def jj(event):
-    hangman()
+    hangaroo()
 
 
 from tkinter import *
 from tkinter import messagebox
 import random
 
-options = ['mayank', 'krushikar', 'anand', 'prem', 'gousepeer', 'sujit', 'eshita']
+options = ['kumar', 'rahul', 'prem', 'keshav', 'sujit', 'eshita']
 
 root = Tk()
 root.geometry('800x500+300+100')
 root.configure(bg='cyan')
-root.title('Hangman Game')
+root.title('Hangaroo')
 
 intro = Label(root, text='Welcome to Game Number 3---Hangaroo ', font=('arial', 25, 'bold'), bg='cyan')
 intro.place(x=100, y=0)
@@ -304,4 +432,159 @@ root.mainloop()
 print()
 print("Pay:", cost)
 
-'''
+import tkinter
+import random
+
+print("********************************************")
+print("Welcome to Game Number 4 --- The Colour Game")
+print("********************************************")
+
+print()
+
+print("Loading the gaming window...................")
+print("Please Wait")
+
+cost = 0
+
+colours = ['Red', 'Blue', 'Green', 'Pink', 'Black',
+           'Yellow', 'Orange', 'White', 'Purple', 'Brown']
+score = 0
+
+timeleft = 30
+
+
+def startGame(event):
+    if timeleft == 30:
+        countdown()
+    nextColour()
+
+
+def nextColour():
+    global score
+    global timeleft
+
+    if timeleft > 0:
+
+        e.focus_set()
+
+        if e.get().lower() == colours[1].lower():
+            score += 1
+
+        e.delete(0, tkinter.END)
+
+        random.shuffle(colours)
+
+        label.config(fg=str(colours[1]), text=str(colours[0]))
+
+        scoreLabel.config(text="Score: " + str(score))
+
+
+def countdown():
+    global timeleft
+
+    if timeleft > 0:
+        timeleft -= 1
+
+        timeLabel.config(text="Time left: "
+                              + str(timeleft))
+
+        timeLabel.after(1000, countdown)
+
+
+root = tkinter.Tk()
+
+root.title("COLORGAME")
+
+root.geometry("375x200")
+
+instructions = tkinter.Label(root, text="Type in the colour"
+                                        "of the words, and not the word text!",
+                             font=('Helvetica', 12))
+instructions.pack()
+
+scoreLabel = tkinter.Label(root, text="Press enter to start",
+                           font=('Helvetica', 12))
+scoreLabel.pack()
+
+timeLabel = tkinter.Label(root, text="Time left: " +
+                                     str(timeleft), font=('Helvetica', 12))
+
+timeLabel.pack()
+
+label = tkinter.Label(root, font=('Helvetica', 60))
+label.pack()
+
+e = tkinter.Entry(root)
+
+root.bind('<Return>', startGame)
+e.pack()
+
+e.focus_set()
+
+root.mainloop()
+
+cost += 100
+
+print("Pay:", cost)
+
+
+# clculator
+
+from tkinter import *
+
+
+def click(event):
+    global sc_value
+    text = event.widget.cget('text')
+    if text == '=':
+        if sc_value.get().isdigit():
+            value = int(sc_value.get())
+        else:
+            try:
+                value = eval(screen.get())
+            except Exception as e:
+                print(e)
+                value = "Error"
+        sc_value.set(value)
+        screen.update()
+    elif text == 'C':
+        sc_value.set("")
+        screen.update()
+    elif text == '<--':
+        sc_value.set(sc_value.get()[:-1])
+        screen.update()
+    else:
+        try:
+            sc_value.set(sc_value.get() + text)
+            screen.update()
+        except Exception:
+            print("something is wrong")
+
+
+root = Tk()
+root.geometry("284x362")
+root.maxsize(284, 362)
+root.minsize(284, 362)
+root.title("CALCULATOR")
+root.configure(background="spring green")
+
+sc_value = StringVar()
+sc_value.set('')
+screen = Entry(root, textvar=sc_value, font="lucida 20 bold")
+screen.place(x=10, y=15, width=261, height=51)
+screen.bind("<Key>", lambda e: "break")
+
+f1 = Frame(root, bg="grey").pack()
+
+xpos = [10, 80, 150, 220, 10, 150, 220]
+ypos = [130, 190, 250, 310, 80]
+x_loop = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 6]
+y_loop = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4]
+wpos = [51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 121, 51, 51, 51]
+fix = ['9', '8', '7', '+', '6', '5', '4', '-', '3', '2', '1', '*', '%', '0', '.', '/', 'C', '<--', '=']
+for y in range(0, 19):  # make All buttons in fix tuple
+    b = Button(f1, text=fix[y], font="lucida 15 bold")
+    b.place(x=xpos[x_loop[y]], y=ypos[y_loop[y]], width=wpos[y], height=41)
+    b.bind("<Button-1>", click)
+
+root.mainloop()
